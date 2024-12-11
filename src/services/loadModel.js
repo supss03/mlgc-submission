3707d944af84b66ce56e96fb19bae7c6f1bdaae3
myalhafiz <91608753+supss03@users.noreply.github.com>
2024-12-11 09:53:49 +0700

    Delete src directory

--- src/services/loadModel.js
+++
@@ -1,5 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/prediction-ml-model/model.json');
-}
-module.exports = loadModel;
